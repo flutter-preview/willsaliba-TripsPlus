54341acb2eb0553163cea818eec6bed6fc5e757b
Will Saliba <salibawill@gmail.com>
2023-07-02 23:58:04 +0930

    forgot to add some screens + changing to snake case file names

---
+++ lib/profile/past_trips.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PastTrips extends StatelessWidget {
+  const PastTrips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
